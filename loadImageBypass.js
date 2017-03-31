e39ceff4adb97b64df99b4ddac7a3051fdefe1f3
tuppercode <tuppermath@gmail.com>
2017-03-31 13:50:40 -0700

    Update loadImageBypass.js

--- loadImageBypass.js
+++ loadImageBypass.js
@@ -6,7 +6,6 @@
  * i-can-t-display-images-dynamically-loaded-from-web-
  * with-p5-js-always-a-cross-domain-issue#Item_3
  */
-var OldLoadImage;
  
 var loadImageErrorOverride = function(errEvt) {
   const pic = errEvt.target;
@@ -18,14 +17,8 @@ var loadImageErrorOverride = function(errEvt) {
 };
 
 var loadImageBypass = function(URL) {
-  OldLoadImage = loadImage;
-  loadImageBypass  = function (URL){
        var img;
-       return OldLoadImage(URL,
+       return loadImage(URL,
             function (pic) { print(img = pic), redraw(); },
             loadImageErrorOverride);
-  };
-};
-loadImage = function(URL) {
-  return loadImageBypass(URL);
 };
