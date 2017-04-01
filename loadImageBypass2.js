9823cdcc830879141c34b2f246c45765332e1a03
tuppercode <tuppermath@gmail.com>
2017-03-31 22:45:01 -0700

    Create loadImageBypass2.js

---
+++ loadImageBypass2.js
@@ -0,0 +1,24 @@
+
+ 
+var loadImageErrorOverride = function(errEvt) {
+  const pic = errEvt.target;
+ 
+  if (!pic.crossOrigin)  return print('Failed to reload ' + pic.src + '!');
+ 
+  print('Attempting to reload it as a tainted image now...');
+  pic.crossOrigin = null, pic.src = pic.src;
+};
+
+var OldLoadImage = loadImage;
+loadImage = function(URL) {
+  return loadImageBypass(URL);
+};
+
+var loadImageBypass = function(URL) {
+       var img;
+       return OldLoadImage(URL,
+            function (pic) { print(img = pic), redraw(); },
+            loadImageErrorOverride);
+  };
+};
+
